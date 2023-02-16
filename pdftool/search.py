cead95b7571ee0430551cdc5e4d5931fa3fae5ae
vrushalichawan <vrushali.chawan@teampicnic.com>
2023-02-16 14:31:45 +0100

    wiring with main.py

--- pdftool/search.py
+++ pdftool/search.py
@@ -2,9 +2,9 @@ from typing import List
 import re
 
 
-def search(search_term: str, content: str) -> List[str] | str:
+def search(search_term: str, content: str) -> List[str]:
     if len(search_term) < 2:
-        return "Insufficient search-term length, minimum two characters!"
+        return ["Insufficient search-term length, minimum two characters!"]
     split_content = split_sentences(content)
     return [sentence for sentence in split_content if search_term in sentence]
 
