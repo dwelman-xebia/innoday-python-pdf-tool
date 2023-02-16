6c6f34c5d67f077b5fc659ae59073af0049fdd4a
Ruben <ruben@Rubens-MacBook-Pro.local>
2023-02-16 13:53:51 +0100

    finish tests

--- tests/search/test_search.py
+++ tests/search/test_search.py
@@ -1,6 +1,35 @@
-from commands.search.search import read_content
+from typing import List
+
+from commands.search import search
+from pypdf import PdfReader
 
 
-def test_load():
-    read_content()
+def read_content(pdf_filepath: str) -> List[str]:
+    reader = PdfReader(pdf_filepath)
+    content = " ".join(page.extract_text().strip() for page in reader.pages)
+    content = ' '.join(content.split())
+    return content
 
+
+def test_search():
+    sentence = "The man walks. I am a nonsesne other sentence?"
+
+    result = search("man walks", sentence)
+
+    assert result == ["The man walks."]
+
+
+def test_insufficient_search_term_length():
+    sentence = "something."
+
+    result = search("s", sentence)
+
+    assert result == "Insufficient search-term length, minimum two characters!"
+
+
+def test_filter_for_all_Is():
+    sentence = "I am a human. I am a developer! Have a nice day"
+
+    result = search("I ", sentence)
+
+    assert result == ["I am a human.", "I am a developer!"]
