81627dbc926667747bb822c7db9d0ec133f76ecc
dwelman-xebia <dwelman@xebia.com>
2023-02-16 10:49:55 +0100

    Add compression flag

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pypdf import PdfReader, PdfWriter
 from argparse import ArgumentParser
 
+from compress.compress import compress_page
+
+import os
+
 parser = ArgumentParser(description=__doc__)
 
 def main():
@@ -13,13 +17,25 @@ def main():
         help="The name for the PDF output file",
         required=False,
     )
+    parser.add_argument(
+        "-c", "--compress",
+        dest="should_compress",
+        action="store_true"
+    )
 
     input_parameters, unknown_input_parameters = parser.parse_known_args()
 
     name = input_parameters.input_pdf_name
     output_name = output_name = name + "-output"
+
     if input_parameters.output_pdf_name is not None:
         output_name = input_parameters.output_pdf_name
+
+    should_compress = False
+    if input_parameters.should_compress is not None:
+        should_compress = input_parameters.should_compress
+        if should_compress is True:
+            print("- Compression Enabled")
     
     if not name.endswith(".pdf"):
         print("File must end with '.pdf' extension")
@@ -29,15 +45,29 @@ def main():
         output_name += ".pdf"
 
     reader = PdfReader(name)
-    number_of_pages = len(reader.pages)
-    print(number_of_pages)
+    
+    input_file_stats = os.stat(name)
+    print('''Input file stats:
+        Size in bytes: {}
+        Pages: {}
+    '''.format(input_file_stats.st_size, len(reader.pages)))
+
     writer = PdfWriter()
 
     for page in reader.pages:
+        if should_compress:
+            page = compress_page(page)
         writer.add_page(page)
 
     with open("{}".format(output_name), "wb") as f:
         writer.write(f)
+
+    output_file_stats = os.stat(output_name)
+    reader = PdfReader(name)
+    print('''Input file stats:
+        Size in bytes: {}
+        Pages: {}
+    '''.format(output_file_stats.st_size, len(reader.pages)))
 
 if __name__ == "__main__":
     main()
