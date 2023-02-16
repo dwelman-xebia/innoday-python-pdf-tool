44b43b39c40ad46edaba57c6208ee2ad750cf9b2
Arjan Molenaar <gaphor@gmail.com>
2023-02-16 13:11:49 +0100

    Move page split code to its own module

--- tests/test_ranges.py
+++ tests/test_ranges.py
@@ -1,4 +1,4 @@
-
+from commands.split import range_to_page_indices
 
 def test_one_page():
     indices = list(range_to_page_indices("5"))
@@ -16,11 +16,3 @@ def test_page_range():
 
     assert indices == [2,3,4]
 
-
-def range_to_page_indices(page_numbers):
-    for pn in page_numbers.split(","):
-        if "-" in pn:
-            f, t = pn.split("-")
-            yield from range(int(f.strip()) - 1, int(t.strip()))
-        else:
-            yield int(pn.strip()) - 1
